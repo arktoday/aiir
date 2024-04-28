391b5f976db60ff1ac2d9a9269cca7c562316af2
Arkadiy <suzdaltsevarkadiy@gmail.com>
2024-04-29 01:44:00 +0300

    New Access Source model

--- access_system/access/models.py
+++ access_system/access/models.py
@@ -2,36 +2,34 @@ import itertools
 import bcrypt
 
 
-class Source:
-    id_iter = itertools.count(0)
+from django.db import models
 
-    id: int
-    name: str
-
-    def __init__(self, name: str):
-        self.id = next(Source.id_iter)
-        self.name = name
+class Source(models.Model):
+    name = models.CharField(max_length=255)
+    
+    class Meta:
+        db_table = 'source'
 
 
-class User:
-    IS_ADMIN: int = 0
+# class User:
+#     IS_ADMIN: int = 0
 
-    id_iter = itertools.count(1)
+#     id_iter = itertools.count(1)
 
-    id: int
-    username: str
-    password_hash: str
+#     id: int
+#     username: str
+#     password_hash: str
 
-    def __init__(self, username: str, password: str):
-        self.id = next(User.id_iter)
-        self.username = username
+#     def __init__(self, username: str, password: str):
+#         self.id = next(User.id_iter)
+#         self.username = username
 
-        salt = bcrypt.gensalt()
-        self.password_hash = bcrypt.hashpw(password, salt)
+#         salt = bcrypt.gensalt()
+#         self.password_hash = bcrypt.hashpw(password, salt)
 
-        self.source_access = []
-        self.is_admin = 0
+#         self.source_access = []
+#         self.is_admin = 0
 
-    @staticmethod
-    def check_pasword(hashed_password: str, password: str) -> bool:
-        return hashed_password == bcrypt.hashpw(password, hashed_password)
+#     @staticmethod
+#     def check_pasword(hashed_password: str, password: str) -> bool:
+#         return hashed_password == bcrypt.hashpw(password, hashed_password)
